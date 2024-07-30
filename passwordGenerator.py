13b3f446b0105f9689c6ff8a9cbf4afcbae0ef94
Marco Coelho <78745589+heim404@users.noreply.github.com>
2024-07-30 04:02:42 +0100

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,67 @@
+import random
+import tkinter as tk
+from tkinter import messagebox
+
+#All possible characters
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lower = 'abcdefghijklmnopqrstuvwxyz'
+numbers = '0123456789'
+symbols = '!@#%&*()_+-=[]{}|;:<>,.?/'
+
+checkLower, checkUpper, checkNumber, checkSymbol = 0, 0, 0, 0
+
+def generate_password():
+    global checkLower, checkUpper, checkNumber, checkSymbol
+    checkLower, checkUpper, checkNumber, checkSymbol = 0, 0, 0, 0
+    try:
+        size = int(pwsize.get())
+        if size <= 0:
+            raise ValueError("Must be a positive number")
+    except ValueError as e:
+        messagebox.showerror("Use a positive number", str(e))
+        return
+    
+    #Generating the character array and then joining it
+    array = [random.choice(upper + lower + numbers + symbols) for _ in range(size)]
+    checkIfPasswordIsStrong(array)
+    password = ''.join(array)
+    entry_password.delete(0, tk.END)
+    entry_password.insert(0, password)
+
+def checkIfPasswordIsStrong(temparray):
+    global checkLower, checkUpper, checkNumber, checkSymbol
+    for char in temparray:
+        if char.isupper():
+            checkUpper += 1
+        if char.islower():
+            checkLower += 1
+        if char.isnumeric():
+            checkNumber += 1
+        if char in symbols:
+            checkSymbol += 1
+
+    #Checks for the requirement of 1 lower,upper, number and symbol        
+    if checkLower == 0 or checkUpper == 0 or checkNumber == 0 or checkSymbol == 0:
+        generate_password()
+    else:
+        pass
+        
+
+#Tkinter UI
+root = tk.Tk()
+root.title("Password Generator")
+label_size = tk.Label(root, text="Enter the size of the password:")
+label_size.pack(pady=5)
+pwsize = tk.Entry(root)
+pwsize.pack(pady=5)
+button_generate = tk.Button(root, text="Generate Password", command=generate_password)
+button_generate.pack(pady=5)
+entry_password = tk.Entry(root, width=50)
+entry_password.pack(pady=5)
+copy_button = tk.Button(root, text="Copy password", command=lambda: root.clipboard_append(entry_password.get()))
+copy_button.pack(pady=2)
+
+#Run APP
+root.mainloop()
+
+
